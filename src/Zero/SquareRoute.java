ff4c2ae024ee28e34011db528436fe563e9e5bd9
Awesozach78 <20171340@archwood.org>
2016-12-16 08:37:26 -0500

    Yes

---
+++ src/Zero/SquareRoute.java
@@ -0,0 +1,7 @@
+package Zero;
+
+/**
+ * Created by 20171340 on 12/16/2016.
+ */
+public class SquareRoute {
+}
---
+++ src/Zero/ZerosBurst.java
@@ -0,0 +1,23 @@
+package Zero;
+
+//I will make you great
+
+import java.awt.*;
+import robocode.Robot;
+
+/**
+ * Created by 20171340 on 12/16/2016.
+ */
+public class ZerosBurst extends Robot{
+    @Override
+    public void run(){
+        ahead(250);
+        back(250);
+        turnGunLeft(350);
+        turnGunRight(350);
+
+
+    }
+
+
+}
