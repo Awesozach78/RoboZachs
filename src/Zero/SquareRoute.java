fbd4f688bd236e3f52388b22669058d3b6bb4bbe
Awesozach78 <20171340@archwood.org>
2016-12-19 08:35:17 -0500

    Yes

--- src/Zero/SquareRoute.java
+++ src/Zero/SquareRoute.java
@@ -1,7 +1,26 @@
 package Zero;
 
+//I will make you great
+
+import java.awt.*;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
 /**
  * Created by 20171340 on 12/16/2016.
  */
-public class SquareRoute {
+public class SquareRoute extends Robot{
+    @Override
+
+
+    public void run(){
+        ahead(250);
+        turnGunRight(360);
+        turnRight(90);
+
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(12);
+    }
 }
